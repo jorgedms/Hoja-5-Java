eb193d56b5e7b3ee37a01c14e1e4445ed5157b3e
jorgedms <man13600@uvg.edu.gt>
2015-08-24 09:33:21 -0600

    1

---
+++ SimOS.py
@@ -0,0 +1,18 @@
+# -*- coding: cp1252 -*-
+#Universidad del Valle de Guatemala
+#Algoritmos y Estructuras de Datos
+#Sección 10
+#
+#Hoja de Trabajo 5
+#
+#SimOS.py
+#
+#Jorge Manrique
+#   13600
+#
+#Christopher Ajú
+#   13171
+#
+
+print ("Simulacion de corrida de programas en un Sistema Operativo de tiempo compartido.")
+
